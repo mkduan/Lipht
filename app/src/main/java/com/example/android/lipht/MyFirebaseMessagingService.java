e47f3ab9dc5b6295dcbe85b22aaabfcc14a6157c
V-Kwong <vincent_kwong578@hotmail.com>
2017-03-19 12:41:14 -0700

    Firebase Stuff

---
+++ app/src/main/java/com/example/android/lipht/MyFirebaseMessagingService.java
@@ -0,0 +1,23 @@
+package com.example.android.lipht;
+
+/**
+ * Created by Vincent on 19/03/17.
+ */
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "MyFMService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " + remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+}
