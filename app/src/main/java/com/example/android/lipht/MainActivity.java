c5d03da78dbaa465e91796238fe98d6608a7af34
Mason Duan <mason.d.97@gmail.com>
2017-03-19 11:26:44 -0700

    Repush

--- app/src/main/java/com/example/android/lipht/MainActivity.java
+++ app/src/main/java/com/example/android/lipht/MainActivity.java
@@ -1,84 +1,69 @@
 package com.example.android.lipht;
 
-import java.util.ArrayList;
-import java.util.Locale;
-
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
-import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class MainActivity extends Activity {
-
-    private TextView txtSpeechInput;
-    private ImageButton btnSpeak;
-    private final int REQ_CODE_SPEECH_INPUT = 100;
-
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private TextView txtOutput;
+    private ImageButton btnMicrophone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
-        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
-
-        // hide the action bar
-        getActionBar().hide();
-
-        btnSpeak.setOnClickListener(new View.OnClickListener() {
-
+        txtOutput = (TextView) findViewById(R.id.txt_output);
+        btnMicrophone = (ImageButton) findViewById(R.id.btn_mic);
+        btnMicrophone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                promptSpeechInput();
+                startSpeechToText();
             }
         });
-
     }
-
     /**
-     * Showing google speech input dialog
+     * Start speech to text intent. This opens up Google Speech Recognition API dialog box to listen the speech input.
      * */
-    private void promptSpeechInput() {
+    private void startSpeechToText() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.speech_prompt));
+                "Speak something...");
         try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
         } catch (ActivityNotFoundException a) {
             Toast.makeText(getApplicationContext(),
-                    getString(R.string.speech_not_supported),
+                    "Sorry! Speech recognition is not supported in this device.",
                     Toast.LENGTH_SHORT).show();
         }
     }
-
     /**
-     * Receiving speech input
+     * Callback for speech recognition activity
      * */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
+            case SPEECH_RECOGNITION_CODE: {
                 if (resultCode == RESULT_OK && null != data) {
-
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    txtSpeechInput.setText(result.get(0));
+                    String text = result.get(0);
+                    txtOutput.setText(text);
                 }
                 break;
             }
-
         }
     }
-
 }
